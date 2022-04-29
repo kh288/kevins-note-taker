ff32556bae789d9f218a58b16b52411f912dd6ec
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-04-28 18:24:29 -0700

    Added POST method with UUID assignment

--- server.js
+++ server.js
@@ -2,16 +2,15 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const database = require('./db/db.json');
-const api = require("./public/assets/js/index.js")
+const { randomUUID } = require('crypto');
 
-var app = express();
 var PORT = process.env.PORT || 3001;
+
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
-app.use('/api', api);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
@@ -21,17 +20,63 @@ app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
+// POST request to add a review
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a review`);
+
+    const { title, text } = req.body;
+
+    // If all the required properties are present
+    if (title && text) {
+        // Variable for the object we will save
+        const newNote = {
+        title,
+        text,
+        id: randomUUID(),
+    };
+
+    // Obtain existing reviews
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+    } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+
+        // Add a new review
+        parsedNotes.push(newNote);
+        reviews = parsedNotes;
+        
+        fs.writeFile(
+        './db/db.json',
+        JSON.stringify(parsedNotes, null, 4),
+            (writeErr) =>
+                writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated reviews!')
+            );
+        }
+    });
+
+        const response = {
+        status: 'success',
+        body: newNote,
+        };
+
+        console.log(response);
+        res.json(response);
+    } else {
+        res.json('Error in posting note');
+    }
+});
+
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-app.get('/api/database', (req, res) => {
-    res.json(database);
-    console.info(`${req.method} request received to get reviews`);
-});
-
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/404.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 })
 
 app.listen(PORT, () =>
